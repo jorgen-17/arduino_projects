25ae0ad60ad68d20bfc7a83f13e8c9fe9a524e70
Jorge Nonell <Jorge_Nonell@ultimatesoftware.com>
2022-02-08 09:43:44 -0500

    implementing basic menu navigation

--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -7,6 +7,8 @@ enum AMPM { AM, PM };
 State state = MENU;
 AMPM ampm = PM;
 
+int ticks = 0;
+
 int hour = 12;
 int minute = 0;
 int second = 0;
@@ -34,8 +36,8 @@ void setup() {
   Serial.begin(9600);
 }
 
-float inputToVoltage (int input) {
-  return input * (5.0 / 1023.0);
+int inputToIntervals (int input, int numberOfIntervals) {
+  return (input  / 1023.0) * numberOfIntervals;
 }
 
 void updateTime () {
@@ -84,12 +86,16 @@ String ampmToString(AMPM ampm) {
   }
 }
 
-void loop() {
-  // int potentiometerInput = analogRead(potentiometerPin);
-  // float inputVoltage = inputToVoltage(potentiometerInput);
-  // Serial.print("Potentiometer Voltage: ");
-  // Serial.println(inputVoltage);
 
+
+void loop() {
+  int potentiometerInput = analogRead(potentiometerPin);
+  int input = inputToIntervals(potentiometerInput, 10);
+  int buttonRead = digitalRead(buttonPin);
+
+  int menuCursor = input % 2 == 0 ? 0 : 1;
+  String line1Cursor = menuCursor == 0 ? ">" : "";
+  String line2Cursor = menuCursor == 1 ? ">" : "";
   String hourString = hour >= 10 ? String(hour) : "0" + String(hour);
   String minuteString = minute >= 10 ? String(minute) : "0" + String(minute);
   String secondString = second >= 10 ? String(second) : "0" + String(second);
@@ -99,19 +105,43 @@ void loop() {
   switch (state) {
     case MENU:
         lcd.setCursor(0, 0);
-        lcd.print(">Time:" + hourString + ":" + minuteString + ":" + secondString + ampmToString(ampm));
+        lcd.print(line1Cursor + "Time:" + hourString + ":" + minuteString + ":" + secondString + ampmToString(ampm));
         lcd.setCursor(0, 1);
-        lcd.print("Date:" + monthString + "/" + dayString + "/" + String(year));
+        lcd.print(line2Cursor + "Date:" + monthString + "/" + dayString + "/" + String(year));
+        if(buttonRead) {
+          if(menuCursor == 0) {
+            state = SET_TIME;
+          }
+          else if (menuCursor == 1) {
+            state = SET_DATE;
+          }
+        }
       break;
     case SET_TIME:
-      // todo set time menu
+        lcd.setCursor(0, 0);
+        lcd.print("Please set time:");
+        lcd.setCursor(0, 1);
+        lcd.print("Time:" + hourString + ":" + minuteString + ":" + secondString + ampmToString(ampm));
+        if(buttonRead) {
+          state = MENU;
+        }
       break;
     case SET_DATE:
-      // todo set date menu
-      break;
+        lcd.setCursor(0, 0);
+        lcd.print("Please set date:");
+        lcd.setCursor(0, 1);
+        lcd.print("Date:" + monthString + "/" + dayString + "/" + String(year));
+        if(buttonRead) {
+          state = MENU;
+        }
+        break;
   }
 
-  delay(1000);
-  updateTime();
+  delay(100);
+  ticks++;
+  if(ticks >= 10) { //update time only once per second
+    ticks = 0;
+    updateTime();
+  }
   lcd.clear();
 }
