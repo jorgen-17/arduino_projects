e926444c2cb51f28d2babeb471f3d544029238ac
Jorge Nonell <Jorge_Nonell@ultimatesoftware.com>
2022-02-09 09:16:02 -0500

    added cursor to the set time and set date menus

--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -8,6 +8,8 @@ State state = MENU;
 AMPM ampm = PM;
 
 int ticks = 0;
+int timeCursor = 0;
+int dateCursor = 0;
 
 int hour = 12;
 int minute = 0;
@@ -86,12 +88,14 @@ String ampmToString(AMPM ampm) {
   }
 }
 
-
-
 void loop() {
   int potentiometerInput = analogRead(potentiometerPin);
   int input = inputToIntervals(potentiometerInput, 10);
   int buttonRead = digitalRead(buttonPin);
+  Serial.print("potentiometer: ");
+  Serial.println(potentiometerInput);
+  Serial.print("buttonRead outside: ");
+  Serial.println(buttonRead);
 
   int menuCursor = input % 2 == 0 ? 0 : 1;
   String line1Cursor = menuCursor == 0 ? ">" : "";
@@ -102,8 +106,25 @@ void loop() {
   String monthString = month >= 10 ? String(month) : "0" + String(month);
   String dayString = day >= 10 ? String(day) : "0" + String(day);
 
+  String hourCursor = timeCursor == 0 ? ">" : "";
+  String minuteCursor = timeCursor == 1 ? ">" : "";
+  String secondCursor = timeCursor == 2 ? ">" : "";
+  String ampmCursor = timeCursor == 3 ? ">" : "";
+
+  String monthCursor = dateCursor == 0 ? ">" : "";
+  String dayCursor = dateCursor == 1 ? ">" : "";
+  String yearCursor = dateCursor == 2 ? ">" : "";
+
+  Serial.print("state: ");
+  Serial.println(state);
+  Serial.print("timeCursor: ");
+  Serial.println(timeCursor);
+  Serial.print("dateCursor: ");
+  Serial.println(dateCursor);
+
   switch (state) {
     case MENU:
+        Serial.println("menu switch case");
         lcd.setCursor(0, 0);
         lcd.print(line1Cursor + "Time:" + hourString + ":" + minuteString + ":" + secondString + ampmToString(ampm));
         lcd.setCursor(0, 1);
@@ -118,28 +139,53 @@ void loop() {
         }
       break;
     case SET_TIME:
+        Serial.println("set time switch case");
         lcd.setCursor(0, 0);
         lcd.print("Please set time:");
         lcd.setCursor(0, 1);
-        lcd.print("Time:" + hourString + ":" + minuteString + ":" + secondString + ampmToString(ampm));
+        lcd.print("Time:" +
+          hourCursor + hourString + ":" +
+          minuteCursor + minuteString + ":" +
+          secondCursor + secondString +
+          ampmCursor + ampmToString(ampm));
+        Serial.print("buttonRead inside set time: ");
+        Serial.println(buttonRead);
         if(buttonRead) {
-          state = MENU;
+          timeCursor++;
+          if(timeCursor >= 4) {
+            timeCursor = 0;
+            state = MENU;
+          }
         }
       break;
     case SET_DATE:
+        Serial.println("set date switch case");
         lcd.setCursor(0, 0);
         lcd.print("Please set date:");
         lcd.setCursor(0, 1);
-        lcd.print("Date:" + monthString + "/" + dayString + "/" + String(year));
+        lcd.print("Date:" +
+          monthCursor + monthString + "/" +
+          dayCursor + dayString + "/" +
+          yearCursor + String(year));
+        Serial.print("buttonRead inside set date: ");
+        Serial.println(buttonRead);
         if(buttonRead) {
-          state = MENU;
+          dateCursor++;
+          if(dateCursor >= 3) {
+            dateCursor = 0;
+            state = MENU;
+          }
         }
-        break;
+      break;
   }
+
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
 
   delay(100);
   ticks++;
-  if(ticks >= 10) { //update time only once per second
+  if(ticks >= 10 && state != SET_TIME) { //update time only once per second
     ticks = 0;
     updateTime();
   }
